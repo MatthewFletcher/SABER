a5c1399c2fb7f8c751f4a9ba96370d49682e62ee
MatthewFletcher <bassmattf@gmail.com>
2017-10-29 14:15:46 -0500

    Adding analysis file

---
+++ winddata.py
@@ -0,0 +1,21 @@
+#Pull information from file at link:
+#README for data https://www1.ncdc.noaa.gov/pub/data/igra/data/igra2-data-format.txt
+
+
+
+#Link to data page
+# https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/
+#File name: USM00072582-data.txt
+
+#IMPORTANT: All section headers begin with a % instead of a #
+
+#Imports
+
+#Read file into python
+with open(home/matt/Desktop/USM00072583-data.txt) as winddatafile
+	lines = winddatafile.readlines()
+	strs = item.strip('\n')
+	
+
+	if strs[0] = %:
+		print("Good")
